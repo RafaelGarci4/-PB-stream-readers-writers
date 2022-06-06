e4afeb4f4def9c900aaf0f5000b43e517e80950c
RafaelGarci4 <rafa.ferreira888@gmail.com>
2022-06-06 11:21:57 -0300

    Adicionando main e arquivos de texto para testes

---
+++ srw/src/FIS/testeLeitura.java
@@ -0,0 +1,12 @@
+package FIS;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class testeLeitura {
+
+    public static void main(String[] args) throws Exception{
+        FileInputStream fis = new FileInputStream("lorem.txt");
+    }
+
+}
