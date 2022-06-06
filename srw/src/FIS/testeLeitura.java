950d36bb0ada33bbb215871e6c3661c3678262d0
RafaelGarci4 <rafa.ferreira888@gmail.com>
2022-06-06 12:03:02 -0300

    commits iniciais; criacao da pasta resources ; criacao do lorem.txt

--- srw/src/FIS/testeLeitura.java
+++ srw/src/FIS/testeLeitura.java
@@ -1,12 +1,26 @@
 package FIS;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 
 public class testeLeitura {
 
-    public static void main(String[] args) throws Exception{
-        FileInputStream fis = new FileInputStream("lorem.txt");
+    public static void main(String[] args) throws IOException {
+
+        //Criando o fluxo de entrada com arquivo
+        FileInputStream fis = new FileInputStream("./resourses/lorem.txt");
+
+        //Melhorando os dados de binario para chars
+        InputStreamReader isr = new InputStreamReader(fis);
+
+        //Agrupando characteres para ler de maneira de mais alto nivel
+        BufferedReader br = new BufferedReader(isr);
+
+        // le a linha
+        String linha = br.readLine();
+
+        System.out.println(linha);
+
+        br.close();
     }
 
 }
